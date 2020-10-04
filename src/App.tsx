1f4956eba4f60f556cd0f52f82c63430bd4989d8
abumalick <abdullah.hilson@gmail.com>
2020-10-04 13:02:50 +0100

    feat: basic implementation of fullcalendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,16 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import { Calendar } from '@fullcalendar/core';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 interface AppProps {}
 
 function App({}: AppProps) {
-  const [count, setCount] = useState(0);
-  setTimeout(() => setCount(count + 1), 1000);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      TEST
+      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
     </div>
   );
 }
